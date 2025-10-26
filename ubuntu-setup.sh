4c4dbfbfbf2895e7ea124eda88967dfc6ea279ed
Jakob Janzen <jakob.janzen80@gmail.com>
2025-10-26 19:03:34 +0100

    added line breaks

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -233,10 +233,14 @@ if dpkg --get-selections "code" | grep --word-regexp "install"; then
   echo "VS Code installed"
 else
   cd /tmp && pwd || echo
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
-  sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor >packages.microsoft.gpg
+  sudo install -D -o root -g root -m 644 packages.microsoft.gpg \
+    /etc/apt/keyrings/packages.microsoft.gpg
   sudo rm -v /etc/apt/sources.list.d/vscode.*
-  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] "\
+    "https://packages.microsoft.com/repos/code stable main" | \
+    sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
   rm -fv packages.microsoft.gpg
   sudo nala update
   sudo nala install -y code
